75075fd79db2261089098476172b8921d8639048
RichardHty <tianyou.richard@gmail.com>
2018-05-27 19:00:29 -0400

    change hard mode

--- color_picker/script.js
+++ color_picker/script.js
@@ -1,26 +1,36 @@
-var questionText = document.querySelector("#question");
+
 var easyMode = document.getElementById("easyMode");
 var hardMode = document.getElementById("hardMode");
-var colorPat = document.getElementById("colorPat");
+
 var colorValue = [];
+var colorResult = [];
 var colors = [];
 var colorAmount = 4;
 var pickedColor = 0;
-var hardRange = 10;
-function generateOneSqure(flag){
-  if(!flag){
+var hardRange = 50;
+var hardFlag = false;
+
+function generateResult(){
+  for(var i = 0;i<3;i++){
+    colorValue[i] = Math.floor(Math.random()*256);
+    colorResult[i]=colorValue[i];
+  }
+  parseRGB();
+}
+function generateOneSqure(){
+  if(hardFlag){
     for(var i = 0;i<3;i++){
-      colorValue[i] = 256;
-      hardRange = 0;
+      colorValue[i] = Math.floor(Math.random()*hardRange*2+(colorResult[i]-hardRange));
+    }
+  }else{
+    for(var i = 0;i<3;i++){
+      colorValue[i] = Math.floor(Math.random()*256);
     }
   }
-  for(var i = 0;i<3;i++){
-    colorValue[i] = Math.floor(Math.random()*(colorValue[i]-hardRange)+hardRange);
-  }
-  setRGB();
+  parseRGB();
 }
 
-function setRGB(){
+function parseRGB(){
   var tempString = "rgb(";
   tempString += colorValue[0]+", ";
   tempString += colorValue[1]+", ";
@@ -29,30 +39,60 @@ function setRGB(){
   colors.push(tempString);
 }
 
-function generateSquares(hardFlag){
-
-  for(var i=0;i<colorAmount;i++){
-    generateOneSqure(false);
-    generateOneSqure(hardFlag);
+function generateSquares(){
+  generateResult();
+  for(var i=1;i<colorAmount;i++){
+    generateOneSqure();
   }
 }
 function displaySquares(){
   for(var i = 0;i<colorAmount;i++){
     var div = document.createElement("div");
     div.classList.add("square");
-    div.style.backgroundColor = colors[i];
-    colorPat.appendChild(div);
+    if(i === pickedColor)
+      div.style.backgroundColor = colors[0];
+    else if(pickedColor !== 0 && i === 0)
+      div.style.backgroundColor = colors[pickedColor];
+    else
+      div.style.backgroundColor = colors[i];
+
+    document.getElementById("colorPat").appendChild(div);
   }
 }
 function pickColor(){
   pickedColor = Math.floor(Math.random()*colorAmount);
-  questionText.textContent = colors[pickedColor];
+  document.querySelector("#question").textContent = colors[0];
 }
+function clearSquares(){
+  var colorPat = document.querySelector("#colorPat");
+  while(colorPat.firstChild){
+    colorPat.removeChild(colorPat.firstChild);
+  }
+  colors.length = 0;
+}
+easyMode.addEventListener("click", function(e){
+  hardFlag = false;
+  colorAmount = 3;
+  clearSquares();
+  generateSquares();
+  pickColor();
+  displaySquares();
+  e.preventDefault();
+});
+hardMode.addEventListener("click", function(e){
+  hardFlag = true;
+  colorAmount = 6;
+  clearSquares();
+  generateSquares();
+  pickColor();
+  displaySquares();
+  e.preventDefault();
+});
+// colorAmount = 6;
+// generateSquares(true);
+// pickColor();
+// displaySquares();
 
-colorAmount = 6;
-generateSquares(true);
-displaySquares();
-pickColor();
 console.log(colors);
 console.log(pickedColor);
 console.log(colorPat.innerHTML);
